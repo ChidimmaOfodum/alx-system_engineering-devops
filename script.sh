27a029b7147b2d2409b74f185f23f91fc0fef45a
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-12-20 16:09:19 -0500

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+##
+# Script to delete remote git branches
+##
+
+# Loop through all remote merged branches
+for branch in $(git branch -r --merged=origin/dev | grep -v HEAD | grep -v dev | grep -v master | sed /\*/d); do
+                echo -e `git show --format="%ci %cr %an" ${branch} | head -n 1` \\t$branch
+                remote_branch=$(echo ${branch} | sed 's#origin/##' )
+done
