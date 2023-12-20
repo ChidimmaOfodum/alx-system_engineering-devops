8588d0c061d113d869b254750bab494cfc85a80c
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-12-20 16:18:09 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,4 +8,5 @@
 for branch in $(git branch -r --merged origin/test | grep -v HEAD | grep -v dev | grep -v master | sed /\*/d); do
                 echo -e `git show --format="%ci %cr %an" ${branch} | head -n 1` \\t$branch
                 remote_branch=$(echo ${branch} | sed 's#origin/##' )
+                git branch --delete ${remote_branch}
 done
