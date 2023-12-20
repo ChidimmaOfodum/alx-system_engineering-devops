52e64e6f5db1735fbbd01bcb861c23827666b5ae
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-12-20 16:16:34 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@
 ##
 
 # Loop through all remote merged branches
-for branch in $(git branch -r --merged test | grep -v HEAD | grep -v dev | grep -v master | sed /\*/d); do
+for branch in $(git branch -r --merged origin/test | grep -v HEAD | grep -v dev | grep -v master | sed /\*/d); do
                 echo -e `git show --format="%ci %cr %an" ${branch} | head -n 1` \\t$branch
                 remote_branch=$(echo ${branch} | sed 's#origin/##' )
 done
