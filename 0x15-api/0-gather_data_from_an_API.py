5873f0b3bf880bf483767ac95b2d1ae2218abaef
Chidimma <chidimmamofodum@gmail.com>
2024-01-02 16:57:19 -0500

    fetching data from an api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Fetches data from JSON placeholder API"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    name_url= f"https://jsonplaceholder.typicode.com/users/{employee_id}/"
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+
+    name = requests.get(name_url).json()
+    todo = requests.get(todo_url).json()
+
+    len = len(todo)
+    completed = 0
+
+    for i in todo:
+        if i['completed']:
+            completed+=1
+
+    print(f"Employee {name['name']} is done with tasks ({completed} / {len})")
+
+for i in todo:
+    print(f"\t {i['title']}") if i['completed'] else None
+
